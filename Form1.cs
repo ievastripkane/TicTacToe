8d8279b51157eb053efb1713576e61b98268f898
Ieva Stripkāne <ieva.stripkane.2002@gmail.com>
2020-05-23 18:17:22 +0300

    version 18:17

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace TicTacToe
     {
 
         bool xPlayerTurn = true;
+        int turnCount = 0;
 
         public Form1()
         {
@@ -28,6 +29,11 @@ namespace TicTacToe
             Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
         }
 
+        private void RestartGame()
+        {
+            InitializeCells();
+            turnCount = 0;
+        }
         private void InitializeCells()
         {
             string labelName;
@@ -41,6 +47,11 @@ namespace TicTacToe
         private void Player_Click(object sender, EventArgs e)
         {
             Label label = (Label)sender;
+            if(label.Text != string.Empty)
+            {
+                return;
+            }
+
             if (xPlayerTurn)
             {
                 label.Text = "X";
@@ -49,7 +60,57 @@ namespace TicTacToe
             {
                 label.Text = "O";
             }
-            xPlayerTurn = !xPlayerTurn;         
+            turnCount++;
+            CheckForWin();
+            CheckForDraw();
+            xPlayerTurn = !xPlayerTurn;            
         }
+
+        private void CheckForWin()
+        {
+            if (
+                (label1.Text == label2.Text && label2.Text == label3.Text && label1.Text != string.Empty) ||
+                (label4.Text == label5.Text && label5.Text == label6.Text && label4.Text != string.Empty) ||
+                (label7.Text == label8.Text && label8.Text == label9.Text && label7.Text != string.Empty) ||
+                (label1.Text == label4.Text && label4.Text == label7.Text && label1.Text != string.Empty) ||
+                (label2.Text == label5.Text && label5.Text == label8.Text && label2.Text != string.Empty) ||
+                (label3.Text == label6.Text && label6.Text == label9.Text && label3.Text != string.Empty) ||
+                (label1.Text == label5.Text && label5.Text == label9.Text && label1.Text != string.Empty) ||
+                (label3.Text == label5.Text && label5.Text == label7.Text && label3.Text != string.Empty)
+                )
+            {
+                GameOver();
+            }
+        }
+
+        private void CheckForDraw()
+        {
+            if(turnCount == 9)
+            {
+                MessageBox.Show("Draw!");
+                RestartGame();
+            }
+        }
+
+   
+        private void GameOver()
+        {
+            string winner;
+            if(xPlayerTurn)
+            {
+                winner = "X";
+            }
+            else
+            {
+                winner = "O";
+            }
+            MessageBox.Show(winner + "wins!");
+            RestartGame();
+        }
+    
+    
+    
+    
     }
+
 }

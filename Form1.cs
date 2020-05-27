f78c2d112605af9b9591211ff33bc8e5de4183f0
Ieva Stripkāne <ieva.stripkane.2002@gmail.com>
2020-05-27 17:59:15 +0300

    version 17:59

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,6 +42,7 @@ namespace TicTacToe
             {
                 labelName = "label" + i;
                 Grid.Controls[labelName].Text = string.Empty;
+                Grid.Controls[labelName].BackColor = Color.Transparent;
             }
         }
 
@@ -61,6 +63,7 @@ namespace TicTacToe
                 label.Text = "O";
             }
             turnCount++;
+            PlaySound();
             CheckForWin();
             CheckForDraw();
             xPlayerTurn = !xPlayerTurn;            
@@ -81,6 +84,59 @@ namespace TicTacToe
             {
                 GameOver();
             }
+        }
+
+        private void WinnerCellsChangeColor()
+        {
+            if (label1.Text == label2.Text && label1.Text == label3.Text && label1.Text != "")
+            {
+                ChangeCellsColors(label1, label2, label3, Color.Purple);
+            }
+            else if (label4.Text == label5.Text && label4.Text == label6.Text && label4.Text != "")
+            {
+                ChangeCellsColors(label4, label5, label6, Color.Purple);
+            }
+            else if (label7.Text == label8.Text && label7.Text == label9.Text && label7.Text != "")
+            {
+                ChangeCellsColors(label7, label8, label9, Color.Purple);
+            }
+            else if (label1.Text == label4.Text && label1.Text == label7.Text && label1.Text != "")
+            {
+                ChangeCellsColors(label1, label4, label7, Color.Purple);
+            }
+            else if (label2.Text == label5.Text && label2.Text == label8.Text && label2.Text != "")
+            {
+                ChangeCellsColors(label2, label5, label8, Color.Purple);
+            }
+            else if (label3.Text == label6.Text && label3.Text == label9.Text && label3.Text != "")
+            {
+                ChangeCellsColors(label3, label6, label9, Color.Purple);
+            }
+            else if (label1.Text == label5.Text && label1.Text == label9.Text && label1.Text != "")
+            {
+                ChangeCellsColors(label1, label5, label9, Color.Purple);
+            }
+            else if (label3.Text == label5.Text && label3.Text == label7.Text && label3.Text != "")
+            {
+                ChangeCellsColors(label3, label5, label7, Color.Purple);
+            }
+
+        }
+
+        private void ChangeCellsColors(Label firstLabel, Label secondLabel,Label thirdLabel, Color color)
+        {
+            firstLabel.BackColor = color;
+            secondLabel.BackColor = color;
+            thirdLabel.BackColor = color;
+
+        }
+
+        private void PlaySound()
+        {
+           System.IO.Stream str = Properties.Resources.click_sound;
+           System.Media.SoundPlayer snd = new System.Media.SoundPlayer(str);
+           snd.Play();
+
         }
 
         private void CheckForDraw()
@@ -104,7 +160,8 @@ namespace TicTacToe
             {
                 winner = "O";
             }
-            MessageBox.Show(winner + "wins!");
+            WinnerCellsChangeColor();
+            MessageBox.Show(winner + " wins!");
             RestartGame();
         }
     
